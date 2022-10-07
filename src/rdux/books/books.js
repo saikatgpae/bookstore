b799ace31129eea5c20233b000c34ea0ef6ff7af
Saikat Chakraborty <saikatgpae@gmail.com>
2022-10-07 22:46:21 +0530

    Add api.

--- src/rdux/books/books.js
+++ src/rdux/books/books.js
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
+// import { createAsyncThunk } from '@reduxjs/toolkit';
 
 export const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 export const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
@@ -22,13 +23,37 @@ const initialState = [{
   id: uuidv4(),
 }];
 
-eslint di
-const API = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/ehEXJIZOdMZTzHuhOZWq/books';
-export const fetchBookThunk = createAsyncThunk(FETCH_BOOK, (action) => {
-  const dispatch = action;
-  fetch(API).then((response) => response.json()).then((data) => {console.log(data);})})
+/* eslint-disable */
 
 
+
+
+
+// const API = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/CmLc1E6jW93Pz1s1S9eG/books';
+
+// const FETCH_BOOK = 'FETCH_BOOK';
+// export const fetchBook = (payload) => ({
+//   type: FETCH_BOOK,
+//   payload,
+// });
+// export const fetchBookThunk = createAsyncThunk(FETCH_BOOK, (action) => {
+  // const dispatch = action;
+  // fetch(API, {
+  //   method: 'POST',
+  //   body: JSON.stringify(
+  //     {"item_id": uuidv4(), "title": "The Great Gatsby", "author": "John Smith", "category": "Fiction"}),
+  //   headers: { 'Content-type': 'application/json; charset=UTF-8' },
+  // }).then((response) => response.json());
+
+  // const scores = async () => {
+  //   const response = await fetch(API);
+  //   const json = await response.json();
+  //   // console.log(json)
+  //   json.forEach((element) => {
+  //     console.log(element)
+  //   });
+  // };
+  // scores();
 
 export default function reduceBook(state = initialState, action) {
   switch (action.type) {
